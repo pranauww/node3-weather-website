d457aac9b2c4f4af3c368bb0d2b75804be667b99
Pranav Bhartiya <bhartiya.pranav@gmail.com>
2024-01-02 21:24:46 +0530

    Init commit

---
+++ src/app.js
@@ -0,0 +1,99 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+const { error } = require('console')
+
+const app = express()
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Pranav Bhartiya'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Pranav Bhartiya'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        name: 'Pranav Bhartiya',
+        helpText: 'You can use this website to determine what the current tempreature is and what tempreature it feels like at any given place in the world. Just enter the name or the zipcode of the desired location to get the weather. If there is a specific city that you want the forecast for, but a different city with the same name shoes up, please specifiy the state to get the desired result!'
+    })
+})
+
+
+app.get('/weather', (req, res) => {
+    if(!req.query.address) {
+        return res.send({
+            error: 'You must provide an address!'
+        })
+    }
+
+    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+        if (error) {
+            return res.send({ error })
+        }
+
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({ error })
+            }
+
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
+    })
+})
+
+app.get('/products', (req, res) => {
+    if(!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term.'
+        })
+    }
+
+    console.log(req.query.search)
+    res.send({
+        products: []
+    })
+})
+
+app.get('/help/*', (req,res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Pranav Bhartiya',
+        errorMessage: 'Help article not found!'
+    })
+})
+
+app.get('*', (req,res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Pranav Bhartiya',
+        errorMessage: 'Page not found!'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.')
+})
